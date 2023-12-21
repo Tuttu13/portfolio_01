e5cfff355c3f52609361e5cc65eca4458346cdaa
Tuttu_13 <willson30426@yahoo.co.jp>
2023-12-22 04:18:36 +0900

    backend model 追加

--- backend/portfolio/models.py
+++ backend/portfolio/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import v
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.IntegerField(validators=[validators.MinValueValidator(0),
+                    validators.MaxValueValidator(100)])
+    name = models.CharField(max_length=500)
+    user_info = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Entry(models.Model):
+    STATUS_DRAFT = "draft"
+    STATUS_PUBLIC = "public"
+    STATUS_SET = (
+            (STATUS_DRAFT, "下書き"),
+            (STATUS_PUBLIC, "公開中"),
+    )
+    title = models.CharField(max_length=128)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_SET, default=STATUS_DRAFT, max_length=8)
+    author = models.ForeignKey(User, related_name='entries', on_delete=models.CASCADE)
